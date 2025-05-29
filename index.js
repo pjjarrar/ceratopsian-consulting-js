afe086a570b53a6358aae5694de64fe0a808baf8
Udunen <cole.huffine@gmail.com>
2025-05-29 16:47:54 -0500

    Ported webpage to Node.js
    JavaScript not ported yet.

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files (HTML, CSS, JavaScript)
+app.use(express.static(__dirname + '/public'));   
+
+// Handle requests for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
